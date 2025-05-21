2b077ffd57dd22fc96714e251063de382476d3e0
AnastasiaTsatsou <anastasia.tsatsou@outlook.com>
2025-05-21 06:03:13 +0300

    Add crud and update models

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import datetime
-from typing import Annotated
+from math import e
+from tkinter import NO
+from typing import Annotated, Optional
 from fastapi import FastAPI, Depends, HTTPException, Query
-from datetime import date
 from enum import Enum
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
@@ -11,21 +12,30 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 
 class CategoryEnum(str, Enum):
-    FOOD = "food"
-    ENTERTAINMENT = "entertainment"
-    TRANSPORT = "transport"
-    HEALTH = "health"
-    OTHER = "other"
+    food = "food"
+    entertainment = "entertainment"
+    transport = "transport"
+    health = "health"
+    other = "other"
 
 
 class Expense(SQLModel, table=True):
     __tablename__ = "expenses"
-    id: int | None = Field(default=None, primary_key=True)
-    name: str | None = Field(default=None, max_length=225)
-    description: str | None = Field(default=None, max_length=400)
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: Optional[str] = Field(default=None, max_length=225)
+    description: Optional[str] = Field(default=None, max_length=400)
     amount: float = Field(gt=0)
-    expense_date: date | None = Field(default=datetime.date.today())
-    category: CategoryEnum = Field(default=CategoryEnum.OTHER)
+    date: Optional[datetime.date] = Field(default_factory=datetime.date.today)
+    category: CategoryEnum = Field(default=CategoryEnum.other)
+
+
+class ExpenseCreate(SQLModel):
+    name: Optional[str] = Field(default=None, max_length=225)
+    description: Optional[str] = Field(default=None, max_length=400)
+    amount: float = Field(gt=0)
+    date: Optional[datetime.date] = Field(default_factory=datetime.date.today)
+    category: CategoryEnum = Field(default=CategoryEnum.other)
 
 
 app = FastAPI()
@@ -51,3 +61,52 @@ def on_startup():
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/expenses/")
+def read_expenses(session: SessionDep) -> list[Expense]:
+    expenses = session.exec(select(Expense)).all()
+    return expenses
+
+
+@app.get("/expenses/{expense_id}")
+def read_expense(expense_id: int, session: SessionDep) -> Expense:
+    expense = session.get(Expense, expense_id)
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+    return expense
+
+
+@app.post("/expenses/")
+def create_expense(expense: ExpenseCreate, session: SessionDep) -> Expense:
+    db_expense = Expense.model_validate(expense)
+    session.add(db_expense)
+    session.commit()
+    session.refresh(db_expense)
+    return db_expense
+
+
+@app.put("/expenses/{expense_id}")
+def update_expense(
+    expense_id: int, expense: ExpenseCreate, session: SessionDep
+) -> Expense:
+    db_expense = session.get(Expense, expense_id)
+    if not db_expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+    expense_data = expense.model_dump(exclude_unset=True)
+    for key, value in expense_data.items():
+        setattr(db_expense, key, value)
+    session.add(db_expense)
+    session.commit()
+    session.refresh(db_expense)
+    return db_expense
+
+
+@app.delete("/expenses/{expense_id}")
+def delete_expense(expense_id: int, session: SessionDep) -> dict:
+    db_expense = session.get(Expense, expense_id)
+    if not db_expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+    session.delete(db_expense)
+    session.commit()
+    return {"message": "Expense deleted successfully"}
